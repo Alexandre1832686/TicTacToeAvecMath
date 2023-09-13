dd5bbb0313f2fcc79f19a820e1ca9d8855b1f936
Alexandre1832686 <1832686@etu.cchic.ca>
2023-09-13 15:08:02 -0400

    alexandre le chad

--- Client/Client/Connecteur.cs
+++ Client/Client/Connecteur.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Client
 {
     public static class Connecteur
     {
+        public static void Connect(string ip)
+        {
 
+        }
     }
 }
--- Client/Client/Controller.cs
+++ Client/Client/Controller.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Automation.Peers;
@@ -14,31 +15,75 @@ namespace Client
         public static int[,] Tableau {  get { return _tableau; } private set { _tableau = value; } }
         
 
-        public static bool ValiderCoup(int x, int y)
+        public static bool ValiderCoup(int x, int y,int joueur)
         {
-            if(ValiderCoupDispo(x,y)&&ValiderCoupLegal(x, y)&&ValiderCoupNombreDeTourCorrect(x, y))
+            if(ValiderCoupDispo(x,y)&&ValiderCoupLegal(x, y)&&ValiderCoupNombreDeTourCorrect(joueur))
             {
-
+                return true;
             }
-            return true;
+            return false;
         }
 
-        public static bool JouerCoup(int x, int y)
+        public static bool JouerCoup(int x, int y,int joueur)
         {
-            ValiderCoup(x, y);
-            return true;
+            if (ValiderCoup(x, y,joueur))
+            {
+                Tableau[x, y] = joueur;
+                return true;
+            }
+            else
+                return false;
         }
         public static bool ValiderCoupDispo(int x, int y)
         {
-            return true;
+            if (Tableau[x, y] == 0)
+                return true;
+            else
+                return false;
         }
         public static bool ValiderCoupLegal(int x, int y)
         {
+            if (x > 2 || x < 0 || y<0 || y>2)
+            {
+                return false;
+            }
+
             return true;
         }
-        public static bool ValiderCoupNombreDeTourCorrect(int x, int y)
+        public static bool ValiderCoupNombreDeTourCorrect(int joueur)
         {
-            return true;
+            int joueur1=0, joueur2=0;
+            if(joueur==1)
+            {
+                joueur1++;
+            }
+            else
+            {
+                joueur2++;
+            }
+            //compte le nombre de coup joué de chaque joueur.
+            for(int i =0;i<3;i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (Tableau[i,j]==1)
+                    {
+                        joueur1++;
+                    }
+                    else if (Tableau[i, j]==2)
+                    {
+                        joueur2++;
+                    }
+                }
+            }
+
+            //verifie que les joueurs on le même nombre de coup joué ou que le joueur 1 en a un de plus
+            if(joueur1 - joueur2 == 0 || joueur1 - joueur2 == 1)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
